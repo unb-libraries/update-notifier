a5d674c7a006c8e96f87f5a05a97358c9c7fd959
Brian Cassidy <brian.cassidy@unb.ca>
2017-03-20 11:52:21 -0300

    rudimentary check for newrelic binary before executing version command

--- update-notifier.py
+++ update-notifier.py
@@ -55,11 +55,13 @@ class DrupalDocker(BaseApp):
     for line in output.splitlines() :
       cols = line.split()
       # Check for newrelic updates
-      nr_ver_line = self._run_ssh(['sudo', 'docker', 'exec', cols[0], 'newrelic-daemon', '--version']).splitlines()[0]
-      nr_ver_string = re.search('\d+.\d+.\d+.\d+', nr_ver_line).group()
-      nr_res_code = subprocess.check_output(['curl', '-s', '-o', '/dev/null', '-I', '-w', '%{http_code}', 'https://download.newrelic.com/php_agent/release/newrelic-php5-' + nr_ver_string + '-linux.tar.gz'])
-      if nr_res_code == '404':
-        results.append([cols[1], nr_ver_string, '', 'NEWRELIC_PHP_VERSION'])
+      nr_exists = self._run_ssh(['sudo', 'docker', 'exec', cols[0], 'which', 'newrelic-daemon']).splitlines()[0]
+      if nr_exists != '':
+        nr_ver_line = self._run_ssh(['sudo', 'docker', 'exec', cols[0], 'newrelic-daemon', '--version']).splitlines()[0]
+        nr_ver_string = re.search('\d+.\d+.\d+.\d+', nr_ver_line).group()
+        nr_res_code = subprocess.check_output(['curl', '-s', '-o', '/dev/null', '-I', '-w', '%{http_code}', 'https://download.newrelic.com/php_agent/release/newrelic-php5-' + nr_ver_string + '-linux.tar.gz'])
+        if nr_res_code == '404':
+          results.append([cols[1], nr_ver_string, '', 'NEWRELIC_PHP_VERSION'])
       updates = self._run_ssh(['sudo', 'docker', 'exec', cols[0], 'drush', 'ups', '--root=/app/html', '--update-backend=drupal', '--format=csv', '--pipe', '2>/dev/null'])
       for update_line in updates.splitlines() :
         update = update_line.rstrip().split(',')
